47fc2136d64722de67ad0e4c79c8581c05678c92
dransingh <rajaransingh@gmail.com>
2023-02-01 21:14:53 +0530

    chore: footer content updated

--- cinemabox/src/components/footer/Footer.jsx
+++ cinemabox/src/components/footer/Footer.jsx
@@ -17,12 +17,11 @@ const Footer = () => {
           <li className="menuItem">FAQ</li>
         </ul>
         <div className="infoText">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
+          Discover the world of cinema at our movie website. Browse through our
+          extensive collection of movies and TV shows, read reviews, and create
+          your own watchlist. With a user-friendly interface and regular
+          updates, we are the ultimate destination for movie enthusiasts.
+          Explore now and join the community of cinema lovers.
         </div>
         <div className="socialIcons">
           <span className="icon">
